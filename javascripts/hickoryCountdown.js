162788e63f4ed2965b29ba47753538b79e19a8f3
euglazer <eugenestapler@gmail.com>
2015-04-05 21:33:59 +1200

    add words'

--- javascripts/hickoryCountdown.js
+++ javascripts/hickoryCountdown.js
@@ -5,19 +5,39 @@ function startHickoryCountdown () {
       screamHickory();
     }, Math.random() * 420);
     $('h1').text(getRandomThought());
-  }, 10000);
+  }, 1000);
 }
 
 var thoughts = [
-  "______________",
-  "am mechanical",
-  "am curved glass",
-  "am tip knitted",
-  "am fish eggs",
-  "am so tired",
-  "am I-5 petrol",
-  "am somehow",
-  "am somewhere"
+  'am mechanical',
+  'am curved glass',
+  'am tip knitted',
+  'am cold air',
+  'am oily hair',
+  '-5 petrol stn',
+  'am somehow',
+  'am somewhere',
+  'am anywhere',
+  'don\'t care',
+  'love chemicals',
+  'for an',
+  'tell me',
+  'eat it quickly',
+  'see it why',
+  'think and think',
+  'say ok and',
+  'smell a salmon',
+  'love algorithms',
+  'want a cat',
+  'eat carpets',
+  'saw it twice',
+  'fed my cat rice',
+  'thought it was',
+  'consume the past',
+  'hope it lasts',
+  'can\'t imagine it',
+  'live quickly',
+  'love gold and'
 ];
 
 function getRandomThought () {
@@ -25,7 +45,7 @@ function getRandomThought () {
 }
 
 function showerTime () {
-  $('#static')[0].play();
+  // $('#static')[0].play();
   $('#typical').hide();
   $('#atypical').show();
 }
@@ -33,5 +53,5 @@ function showerTime () {
 function screamHickory () {
   $('#atypical').hide();
   $('#typical').show();
-  $('#static')[0].pause();
+  // $('#static')[0].pause();
 }
