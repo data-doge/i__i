20ca1e85a5ef76b6cd6503f017c3d764be7e7c24
euglazer <eugenestapler@gmail.com>
2015-04-05 18:03:13 +1200

    font and thoughts

--- index.js
+++ index.js
@@ -40,9 +40,21 @@ $(document).ready(function() {
     }
   }, 10);
 
+  var $thought = $('h1');
   var static = $('#static')[0];
   $typical = $('#typical');
   $atypical = $('#atypical');
+  var thoughts = [
+    "______________",
+    "am mechanical",
+    "am curved glass",
+    "am tip knitted",
+    "am fish eggs",
+    "am so tired",
+    "am I-5 petrol",
+    "am somehow",
+    "am somewhere"
+  ];
 
   var hickoryCountdown = setInterval(function () {
 
@@ -54,9 +66,11 @@ $(document).ready(function() {
       $atypical.hide();
       $typical.show();
       static.pause();
-    }, Math.random() * 200);
+    }, Math.random() * 420);
 
-  }, 15000);
+    $thought.text(thoughts[Math.floor(Math.random() * thoughts.length)]);
+
+  }, 10000);
 
 
 
