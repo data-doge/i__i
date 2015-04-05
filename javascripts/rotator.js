e34f5daaeef0056964ec71ba178da78eb2d6bdae
euglazer <eugenestapler@gmail.com>
2015-04-05 19:22:57 +1200

    refactor

--- javascripts/rotator.js
+++ javascripts/rotator.js
@@ -4,14 +4,13 @@ function startRotating () {
   $typical = $('#typical');
 
   var rotator = setInterval(function () {
+
     $typical.css({
       '-webkit-transform' : 'rotate(' + j + 'deg)'
-    })
-    if (j < 360) {
-      j++;
-    } else {
-      j = 0;
-    }
+    });
+
+    j < 360 ? j++ : j = 0;
+
   }, 10);
 
 }
